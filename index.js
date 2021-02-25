a6e4da60f32768ee667fae9342483f561d8aca96
Kennn-dev <nhatong2015@gmail.com>
2021-02-25 16:14:42 +0700

    update cursor smooth scroll

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-var swiper = new Swiper(".swiper-container", {
+let $cursor = $(".cursor");
+
+const $listHover = $("span ,h1 ,h3");
+
+function moveCircle(e) {
+  TweenLite.to($cursor, 0.3, {
+    x: e.clientX,
+    y: e.clientY,
+  });
+  console.log({ x: e.clientX, y: e.clientY });
+}
+// {
+//   x: e.clientX,
+//   y: e.clientY,
+// }
+function hoverFunc(e) {
+  TweenLite.to($cursor, 0.3, {
+    scale: 3,
+  });
+}
+function unHoverFunc(e) {
+  TweenLite.to($cursor, 0.3, {
+    scale: 1,
+  });
+}
+
+let swiper = new Swiper(".swiper-container", {
   slidesPerView: 3,
   spaceBetween: 10,
   loop: true,
@@ -11,3 +37,10 @@ var swiper = new Swiper(".swiper-container", {
     clickable: true,
   },
 });
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
+
+$(window).on("mousemove", moveCircle);
+$listHover.hover(hoverFunc, unHoverFunc);
